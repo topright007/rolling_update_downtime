12d038fb3a9df8d27afae1041a6e8c4f004c8454
topright <elstano@gmail.com>
2024-03-04 10:19:07 +0300

    add serialization as json and display of measurement params

--- ipnb/measure.sh
+++ ipnb/measure.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+python3 ./calc_downtime.py --restart-date "2023-10-02 10:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 40 --grace-period-sec 1800 &
+python3 ./calc_downtime.py --restart-date "2023-10-02 11:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 40 --grace-period-sec 1800 &
+python3 ./calc_downtime.py --restart-date "2023-10-02 12:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 40 --grace-period-sec 1800 &
+wait
 
-python3 ./calc_downtime.py --restart-date "2023-10-02 10:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 15 --grace-period-sec 3600
-python3 ./calc_downtime.py --restart-date "2023-10-02 11:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 15 --grace-period-sec 3600
-python3 ./calc_downtime.py --restart-date "2023-10-02 12:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 15 --grace-period-sec 3600
-python3 ./calc_downtime.py --restart-date "2023-10-02 13:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 15 --grace-period-sec 3600
-python3 ./calc_downtime.py --restart-date "2023-10-02 14:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 15 --grace-period-sec 3600
-python3 ./calc_downtime.py --restart-date "2023-10-02 15:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 15 --grace-period-sec 3600
-python3 ./calc_downtime.py --restart-date "2023-10-02 16:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 15 --grace-period-sec 3600
-python3 ./calc_downtime.py --restart-date "2023-10-02 17:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 15 --grace-period-sec 3600
-python3 ./calc_downtime.py --restart-date "2023-10-02 18:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 15 --grace-period-sec 3600
-python3 ./calc_downtime.py --restart-date "2023-10-02 19:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 15 --grace-period-sec 3600
-python3 ./calc_downtime.py --restart-date "2023-10-02 20:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 15 --grace-period-sec 3600
-python3 ./calc_downtime.py --restart-date "2023-10-02 21:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 15 --grace-period-sec 3600
-python3 ./calc_downtime.py --restart-date "2023-10-02 22:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 15 --grace-period-sec 3600
-python3 ./calc_downtime.py --restart-date "2023-10-02 23:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 15 --grace-period-sec 3600
-python3 ./calc_downtime.py --restart-date "2023-10-03 00:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 15 --grace-period-sec 3600
+python3 ./calc_downtime.py --restart-date "2023-10-02 10:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 40 --grace-period-sec 900 &
+python3 ./calc_downtime.py --restart-date "2023-10-02 11:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 40 --grace-period-sec 900 &
+python3 ./calc_downtime.py --restart-date "2023-10-02 12:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 40 --grace-period-sec 900 &
+wait
+
+#python3 ./calc_downtime.py --restart-date "2023-10-02 13:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 40 --grace-period-sec 3600 &
+#python3 ./calc_downtime.py --restart-date "2023-10-02 14:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 40 --grace-period-sec 3600 &
+#python3 ./calc_downtime.py --restart-date "2023-10-02 15:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 40 --grace-period-sec 3600 &
+#wait
+#python3 ./calc_downtime.py --restart-date "2023-10-02 16:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 40 --grace-period-sec 3600 &
+#python3 ./calc_downtime.py --restart-date "2023-10-02 17:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 40 --grace-period-sec 3600 &
+#python3 ./calc_downtime.py --restart-date "2023-10-02 18:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 40 --grace-period-sec 3600 &
+#wait
+#python3 ./calc_downtime.py --restart-date "2023-10-02 19:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 40 --grace-period-sec 3600 &
+#python3 ./calc_downtime.py --restart-date "2023-10-02 20:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 40 --grace-period-sec 3600 &
+#python3 ./calc_downtime.py --restart-date "2023-10-02 21:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 40 --grace-period-sec 3600 &
+#wait
+#python3 ./calc_downtime.py --restart-date "2023-10-02 22:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 40 --grace-period-sec 3600 &
+#python3 ./calc_downtime.py --restart-date "2023-10-02 23:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 40 --grace-period-sec 3600 &
+#python3 ./calc_downtime.py --restart-date "2023-10-03 00:00:00,000"   --policy RandomIslandLeastLoadedNewNodePolicy  --dt-calc-model IntegratingDTClacModel   --disruption-budget 40 --grace-period-sec 3600 &
